578485f044d89eb324bdb566411780a2f61aa47c
mutuku1998 <martinmutuku1998@gmail.com>
2023-07-13 12:40:51 +0300

    kilifi-bodaboda

---
+++ bodaboda/src/Pages/Ridersdetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ridersdetails = () => {
+  return (
+    <div>Ridersdetails</div>
+  )
+}
+
+export default Ridersdetails
---
+++ bodaboda/src/Pages/Riderslisting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Riderslisting = () => {
+  return (
+    <div>Riderslisting</div>
+  )
+}
+
+export default Riderslisting
