2100993b68a6b53bb288917a6974eb5a123915a9
mutuku1998 <martinmutuku1998@gmail.com>
2023-07-15 17:15:04 +0300

    footer

--- bodaboda/src/Components/Footer/Footer.jsx
+++ bodaboda/src/Components/Footer/Footer.jsx
@@ -14,20 +14,26 @@ const quickLinks =[
   },
   {
     path:'/riders',
-    display: 'Car Listing'
+    display: 'Riders Listing'
   },
   {
     path :'/contact',
     display:'Contact'
+  },
+  {
+    path:'/blogs',
+    display:'Blogs'
   }
 ]
 const Footer = () => {
+  const date = new Date()
+  const year = date.getFullYear()
   return (
   <footer className='footer'>
     <Container>
       <Row>
         <Col lg-4 md-4 sm-12>
-        <div className='logo'>
+        <div className='logo footer-logo'>
     <h1>
         <Link to='/' className='d-flex align-items-center
         gap-3'>
@@ -41,15 +47,15 @@ We want people on our team who reflect our values of outstanding service and utm
 which is why we’re always searching for new like-minded Bodaboda Riders to join our growing fleet
 </p>
         </Col>
-        <Col lg-4 md-4 sm-6>
+        <Col lg='2' md='4' sm='6'>
           <div className='mb-4'>
          
-         <h5 className='footer-link-title'>
+         <h5 className='footer-link-title '>
           Quick Links</h5>
 <ListGroup>
   {
     quickLinks.map ((item,index) =>(
-      <ListGroupItem key={index} className='p-0 mt-3' >
+      <ListGroupItem key={index} className= ' quick-links p-0 mt-3 ' >
 <Link to ={item.path}>
 {item.display}
 </Link>
@@ -59,6 +65,44 @@ which is why we’re always searching for new like-minded Bodaboda Riders to joi
 </ListGroup>
           </div>
 
+        </Col>
+        <Col lg='3'md='4'sm='6'>
+          <div className='mb-4'>
+          <h5 className='footer-link-title mb-4'>
+          Head office</h5>
+          <p className='office-info'>
+            Kilifi Town , Kenya
+          </p>
+          <p className='office-info'>
+          Phone: +254741934896
+          </p>
+          <p className='office-info'>
+         Email: Kilifibodaboda@gmail.com
+          </p>
+          <p className='office-info'>
+           Office Time: 8.am - 7pm
+          </p>
+          </div>
+        </Col>
+        <Col lg='3' md='4'>
+          <div className='mb-4'>
+          <h5 className='footer-link-title'>
+          Newsletter</h5>
+          <p className='section-description'>
+            Subscribe our newsletter</p>
+             <div className='newsletter'>
+              <input type='email' placeholder='Enter email address'/>
+              <span><i class="ri-send-plane-fill"></i></span>
+              </div>        
+                </div>
+        </Col>
+        <Col lg='12'>
+          <div className='footer-bottom'>
+            <p className='section-description d-flex align-items-center justify-content-center
+            gap-1 pt-4'>
+            <i class="ri-copyright-fill"></i>Copyright {year}, Developed by martin kavindu.
+All rights reserved </p>
+          </div>
         </Col>
       </Row>
     </Container>
